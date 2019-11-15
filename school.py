ec786f2bf8bf6a51eea2e546650624643902badd
Melvin Gonzalez Jr <mgonzalez713@gmail.com>
2019-11-15 00:28:50 -0600

    Completed

--- school.py
+++ school.py
@@ -1,1 +1,16 @@
 class School:
+	def __init__(self, name=None,  roster={}):
+		self.name = name
+		self.roster = roster
+        
+	def add_student(self, full_name, grade):
+		self.roster.setdefault(grade, []).append(full_name)
+        
+	def grade(self, grade):
+		return self.roster[grade]
+    
+	def sort_roster(self):
+		new_dict = self.roster
+		for key in new_dict:
+			new_dict[key].sort()
+		return new_dict
